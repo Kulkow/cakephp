0cd0d11dc8036f0015813f67379005e470aa5988
k1785 <k1785@rambler.ru>
2016-04-03 20:47:35 +0300

    'editcomposer2'

--- composer.php
+++ composer.php
@@ -4,16 +4,21 @@ foreach($scan as $dir){
     if(! in_array($dir, ['.','..'])){
         if(! file_exists(__DIR__.'/'.$dir.'/composer.json')){
             $json = [
-                "name" => strtolower($dir),
+                "name" => $dir,
+                "type" => "cakephp-plugin",
                 "description" => $dir." IS2B",
                 "version" => "1.0.0",
                 "keywords" => ["cakephp", strtolower($dir)],
-                "require" => ["php" => ">=5.4.0"]
+                "require" => [
+                    "php" => ">=5.4.0",
+                    "composer/installers" => "*",
+                ]
             ];
-            $composer = json_encode($json,JSON_PRETTY_PRINT);
+            $composer = json_encode($json,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
             file_put_contents(__DIR__.'/'.$dir.'/composer.json',$composer);
             print_r($composer);
         }else{
+            //unlink(__DIR__.'/'.$dir.'/composer.json');
             //echo __DIR__.'/'.$dir.'/composer.js';
         }
     }
